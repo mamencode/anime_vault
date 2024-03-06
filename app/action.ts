f4e4147a246fbbeaddb13c330e18babfdc16ffca
mamencode2 <mamencode2@gmail.com>
2024-03-06 15:52:52 +0000

    fdh

--- app/action.ts
+++ app/action.ts
@@ -1,7 +1,7 @@
 'use server'
 
-export const fetchAnime= async()=>{
-   const response = await fetch(`https://shikimori.one/api/animes`) 
+export const fetchAnime= async(page:number)=>{
+   const response = await fetch(`https://shikimori.one/api/animes?page=${page}&limit=10`) 
    const data = await response.json()
    return data
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,12 @@ import { data } from "./_data";
 import { fetchAnime } from "./action";
 
 async function Home() {
-  const resdata = await fetchAnime()
-  console.log(resdata)
+  //const resdata = await fetchAnime()
+  //console.log(resdata)
   return (
     <main className="sm:p-16 py-16 px-8 flex flex-col gap-10">
       <h2 className="text-3xl text-white font-bold">Explore Anime</h2>
 
-      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
-        {resdata.map((item: AnimeProp, index:number) => (
-          <AnimeCard key={item.id} anime={item} index={index} />
-        ))}
-      </section>
       <LoadMore />
     </main>
   );
